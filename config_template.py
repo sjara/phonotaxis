56e7eea53496d8411dad115dfa0b47b919c566a0
sjara <sjara@uoregon.edu>
2025-10-22 17:02:23 -0700

    Added a few more parameters.

--- config_template.py
+++ config_template.py
@@ -4,18 +4,23 @@ Configuration file for phonotaxis application in a specific rig.
 
 CAMERA_INDEX = 0
 
+HARDWARE_INTERFACE = 'arduino'  # Options: 'arduino', 'emulator'
+#HARDWARE_INTERFACE = 'emulator'  # Options: 'arduino', 'emulator'
+
 ARDUINO_PORT = '/dev/ttyACM0' # COM4
+ARDUINO_SAMPLING_INTERVAL = 10  # in milliseconds
+ARDUINO_INPUTS_INVERTED = True  # Set to True if using pull-up resistors
 
 # Input pins need to be defined in order from 0 to N-1
 INPUT_PINS = {
-    'port1': 0,  # Analog pin A0
-    'port2': 1,  # Analog pin A1
+    'L': 0,  # Analog pin A0
+    'R': 1,  # Analog pin A1
 }
 
 # Output pins can be defined in any order
 OUTPUT_PINS = {
-    'water1': 13,  # Digital pin for water delivery 1
-    'water2': 12,  # Digital pin for water delivery 2
+    'ValveL': 13,  # Digital pin for water delivery 1
+    'ValveR': 12,  # Digital pin for water delivery 2
 }
 
 # You can list available sound devices in Python with:
