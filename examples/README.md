4634c1ff5e4c4111f87c8a2cc528d83862956a39
sjara <sjara@uoregon.edu>
2025-10-25 17:41:47 -0700

    Additional examples.

--- examples/README.md
+++ examples/README.md
@@ -1,10 +1,16 @@
-# Examples and demos
+# Examples and demos of the core modules
 
 1. `example_gui_minimal.py`: Tests Qt installation. You should see a window with minimal text on it.
 2. `example_video_tracking.py`: Demo video and object tracking. You should see a window with b/w video from your camera and red dot tracking a black object.
 3. `example_gui_control_and_params.py`: Example of adding the session controller and parameters. This will show a window with a green "Start" button and some text boxes for parameters.
 4. `example_arduino_analog_inputs.py`: Plot the Arduino analog inputs.
 5. `example_arduino_control_widget.py`: Monitor whether Arduino analog inputs cross threshold and control Arduino digital outputs.
+6. `example_statematrix.py`: Example of how to create a state matrix.
+7. `example_statemachine.py`: Example of how to set up a state machine (without the controller module).
+
+# Examples of loading data
+1. `example_read_video_frame.py`: Read and display the first video frame using OpenCV.
+2. `example_read_centroid_file.py`: Load and plot centroid data.
 
 ## Other
 * `example_sound_on_roi.py`: Simulated task where a sound is presented when an object enters the initiation zone. The user must indicate with arrow keys whether sound came from left or right. It saves the video (as mp4) and the centroid (as CSV).
--- examples/example_statematrix.py
+++ examples/example_statematrix.py
@@ -1,42 +1,33 @@
 """
-Test statematrix module.
+Example of how to use the statematrix module.
 """
 
 from phonotaxis import statematrix
+import numpy as np
 
-if 0:
-    sm = statematrix.StateMatrix(inputs=['C', 'L', 'R'],
-                        outputs=['centerLED', 'leftLED', 'rightLED'])
+INPUTS = ['IZ', 'L', 'R']
+OUTPUTS = ['ValveL', 'ValveR']
 
-    sm.add_state(name='wait_for_cpoke', statetimer=2,
-                transitions={'Cin':'stim_on', 'Tup':'stim_on'},
-                outputsOff=['centerLED'])
-    sm.add_state(name='stim_on', statetimer=0.5,
-                transitions={'Tup':'wait_for_cpoke'},
-                outputsOn=['centerLED'])
-    print(sm)
+SOUND_ID = 1
+CORRECT_PORT = 'R'  # For example, right port
+VALVE_DURATION = 0.2
 
+sm = statematrix.StateMatrix(inputs=INPUTS, outputs=OUTPUTS)
 
-if 1:
-    import numpy as np
-    valve_duration = 0.2
-    correct_port = 'R'  # 'L' or 'R'
-    INPUTS = ['IZ', 'L', 'R']
-    OUTPUTS = ['ValveL', 'ValveR']
-    sm = statematrix.StateMatrix(inputs=INPUTS, outputs=OUTPUTS)
-    sm.add_state(name='wait_for_init_zone', statetimer=np.inf,
-                        transitions={'IZin':'play_sound'},
-                        outputsOff=['ValveL', 'ValveR'])
-    sm.add_state(name='play_sound', statetimer=0.5,
-                        transitions={'Tup':'wait_for_response'})
-    sm.add_state(name='wait_for_response', statetimer=5,
-                        transitions={correct_port+'in':'rewardOn',
-                                    'Tup':'END'},
-                        outputsOff=['ValveL', 'ValveR'])
-    sm.add_state(name='rewardOn', statetimer=valve_duration,
-                        transitions={'Tup':'rewardOff'},
-                        outputsOn=['Valve'+correct_port])
-    sm.add_state(name='rewardOff', statetimer=0,
-                        transitions={'Tup':'END'},
-                        outputsOff=['Valve'+correct_port])
-    print(sm)
+sm.add_state(name='wait_for_init_zone', statetimer=np.inf,
+                    transitions={'IZin':'play_sound'},
+                    outputsOff=['ValveL', 'ValveR'])
+sm.add_state(name='play_sound', statetimer=0.5,
+                    transitions={'Tup':'wait_for_response'},
+                    integerOut=SOUND_ID)
+sm.add_state(name='wait_for_response', statetimer=5,
+                    transitions={CORRECT_PORT+'in':'rewardOn', 'Tup':'END'},
+                    outputsOff=['ValveL', 'ValveR'])
+sm.add_state(name='rewardOn', statetimer=VALVE_DURATION,
+                    transitions={'Tup':'rewardOff'},
+                    outputsOn=['Valve'+CORRECT_PORT])
+sm.add_state(name='rewardOff', statetimer=0,
+                    transitions={'Tup':'END'},
+                    outputsOff=['Valve'+CORRECT_PORT])
+
+print(sm)
